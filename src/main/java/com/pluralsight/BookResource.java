bcf35b5711adbf151338c7163dc66e3518a2e991
Ashmeet Rekhi <ashrekhi@gmail.com>
2017-08-18 09:13:01 -0700

    Entity tag/Conditional get additions

--- src/main/java/com/pluralsight/BookResource.java
+++ src/main/java/com/pluralsight/BookResource.java
@@ -57,6 +57,7 @@ public class BookResource {
     //to set json as default
     @Produces({"application/json;qs=1", "application/xml;qs=0.5"})
     @ManagedAsync
+    //https://stackoverflow.com/questions/11138215/entitytag-value-caching-comparison-how-to-in-jersey
     public void getBook(@PathParam("id") String id, @Suspended final AsyncResponse response){
         //response.resume(dao.getBook(id));
         ListenableFuture<Book> bookFuture = dao.getBookAsync(id);
@@ -64,14 +65,20 @@ public class BookResource {
             @Override
             public void onSuccess(Book addedBook) {
                // response.resume(addedBook);
-               // To check if the response has been changed (Conditional GET)
+               // To check if the response has been changed (Conditional GET), generate a unique tag
                EntityTag entityTag = generateEntityTag(addedBook);
+                // the following method call will result in Jersey checking the headers of the
+                // incoming request, comparing them with the entity tag generated for
+                // the current version of the resource generates "304 Not Modified" response
+                // if the same. Otherwise returns null.
                Response.ResponseBuilder rb = request.evaluatePreconditions(entityTag);
                //If response has not changed
                if (rb != null) {
+                   //304 not modified
                    response.resume(rb.build());
                //If response has been modified
                } else {
+                   // return the current version of the resource with the corresponding tag
                    response.resume(Response.ok().tag(entityTag).entity(addedBook).build());
                }
             }
@@ -103,7 +110,7 @@ public class BookResource {
         });
     }
 
-    //For conditional Get, it is basically caching of the reponse, in a new entity.
+    //For conditional Get, it is basically caching of the response, in a new entity.
     EntityTag generateEntityTag(Book book) {
         return new EntityTag(DigestUtils.md5Hex(book.getAuthor() +
         book.getTitle() + book.getPublished() + book.getExtras()));
